d4b12e762648ccf60232d4c34348f6512145d21e
azizulhoq953 <azizulhoq2041@gmail.com>
2025-12-25 12:43:11 +0600

    sc

--- src/helpers/checkObjectIdZodSchemaHelper.ts
+++ src/helpers/checkObjectIdZodSchemaHelper.ts
@@ -4,3 +4,4 @@ export const objectIdZodSchema = (value: string) =>
     z.string({ required_error: `${value}` }).refine((val) => /^[a-fA-F0-9]{24}$/.test(val), {
         message: `Invalid ${value} format`,
     });
+    
